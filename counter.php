7b32c1b6c64c98ce40a434d044435255500e5247
nabm <nbasumullick11@gmail.com>
2011-10-29 20:12:04 -0300

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,18 +1,16 @@
 <?php
-
-	if (file_exists('count_file.txt')) 
+	if (file_exists('count_f.txt')) 
 	{
-		$fil = fopen('count_file.txt', r);
-		$dat = fread($fil, filesize('count_file.txt')); 
+		$fil = fopen('count_f.txt', r);
+		$dat = fread($fil, filesize('count_f.txt')); 
 		echo $dat+1;
 		fclose($fil);
-		$fil = fopen('count_file.txt', w);
+		$fil = fopen('count_f.txt', w);
 		fwrite($fil, $dat+1);
 	}
-
 	else
 	{
-		$fil = fopen('count_file.txt', w);
+		$fil = fopen('count_f.txt', w);
 		fwrite($fil, 1);
 		echo '1';
 		fclose($fil);
